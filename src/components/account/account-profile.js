fa66f65b3e3421457f5555e9df5b3bf61cbe4870
vodinhphuc1234 <phucatoniegriezman@gmail.com>
2022-07-31 19:00:40 +0700

    add avt

--- src/components/account/account-profile.js
+++ src/components/account/account-profile.js
@@ -6,58 +6,46 @@ import {
   CardActions,
   CardContent,
   Divider,
-  Typography
-} from '@mui/material';
+  Typography,
+} from "@mui/material";
 
-
-export const AccountProfile = ({user, ...rest}) => (
-  <Card {...rest}>
-    <CardContent>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column'
-        }}
-      >
-        <Avatar
-          src="https://w7.pngwing.com/pngs/223/244/png-transparent-computer-icons-avatar-user-profile-avatar-heroes-rectangle-black.png"
+export const AccountProfile = ({ user, ...rest }) => {
+  console.log(user);
+  return (
+    <Card {...rest}>
+      <CardContent>
+        <Box
           sx={{
-            height: 64,
-            mb: 2,
-            width: 64
+            alignItems: "center",
+            display: "flex",
+            flexDirection: "column",
           }}
-        />
-        <Typography
-          color="textPrimary"
-          gutterBottom
-          variant="h5"
         >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {`${user.address.suite} ${user.address.street} ${user.address.city}`}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {user.address.zipcode}
-        </Typography>
-      </Box>
-    </CardContent>
-    <Divider />
-    <CardActions>
-      <Button
-        color="primary"
-        fullWidth
-        variant="text"
-      >
-        Upload picture
-      </Button>
-    </CardActions>
-  </Card>
-);
+          <Avatar
+            src="https://w7.pngwing.com/pngs/223/244/png-transparent-computer-icons-avatar-user-profile-avatar-heroes-rectangle-black.png"
+            sx={{
+              height: 64,
+              mb: 2,
+              width: 64,
+            }}
+          />
+          <Typography color="textPrimary" gutterBottom variant="h5">
+            {user.name}
+          </Typography>
+          <Typography color="textSecondary" variant="body2">
+            {`${user.address.suite} ${user.address.street} ${user.address.city}`}
+          </Typography>
+          <Typography color="textSecondary" variant="body2">
+            {user.address.zipcode}
+          </Typography>
+        </Box>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button color="primary" fullWidth variant="text">
+          Upload picture
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
