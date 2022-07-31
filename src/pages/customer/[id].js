a420244e6561305b79b6d23816d0370d848fb435
vodinhphuc1234 <phucatoniegriezman@gmail.com>
2022-07-31 18:38:41 +0700

    Fix customer detail page

--- src/pages/customer/[id].js
+++ src/pages/customer/[id].js
@@ -25,10 +25,10 @@ const CustomerDetail = ({ customer }) => {
           </Typography>
           <Grid container spacing={3}>
             <Grid item lg={4} md={6} xs={12}>
-              <AccountProfile />
+              <AccountProfile user={customer}  />
             </Grid>
             <Grid item lg={8} md={6} xs={12}>
-              <AccountProfileDetails />
+              <AccountProfileDetails user={customer}  />
             </Grid>
             <Grid item lg={12} mt={10}>
               <Divider />
