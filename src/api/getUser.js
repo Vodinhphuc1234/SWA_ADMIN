3978f5506a94012953a707a0e8257b3171348440
vodinhphuc1234 <phucatoniegriezman@gmail.com>
2022-07-31 18:43:20 +0700

    add avt

--- src/api/getUser.js
+++ src/api/getUser.js
@@ -2,5 +2,6 @@ import BaseAPI from ".";
 
 export const getUser = async (props) => {
   let user = await BaseAPI.get("/users", { ...props });
+  if (!user.avatar) user.avatar = "https://w7.pngwing.com/pngs/223/244/png-transparent-computer-icons-avatar-user-profile-avatar-heroes-rectangle-black.png"
   return user.data[0];
 };
